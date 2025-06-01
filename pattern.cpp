394b9bc58136b90db2192b4a20519ea688a821ef
manish0502525 <manish7773148@gmail.com>
2025-06-01 14:46:18 +0530

    new update

---
+++ pattern.cpp
@@ -0,0 +1,169 @@
+// #include<stdio.h>
+// int main(){
+//     int n;
+//     printf("enter a numbber ");
+//     scanf("%d",&n);
+//     for(int i=1;i<=n;i++){
+//         for(int j=1;j<=i;j++){--------------
+//             printf("*");
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+
+
+// out put   
+
+// enter a numbber 7
+// *
+// **
+// ***
+// ****
+// *****
+// ******
+// *******
+// ********
+// =============================================================
+
+
+// #include<stdio.h>
+// int main(){
+//     int n;
+//     printf("enter a numbber ");
+//     scanf("%d",&n);
+//     for(int i=17
+//         7;i<=n;i++){
+//         for(int j=0;j<=n+1-i;j++){--------
+//             printf("*");
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+// out put=================================================
+
+// enter a numbber 7
+// ********
+// *******
+// ******
+// *****
+// ****
+// ***
+// **
+
+// =================================================================
+// #include<stdio.h>
+// int main(){
+//     int n;
+//     printf("enter a numbber ");
+//     scanf("%d",&n);
+//     for(int i=1;i<=n;i++){
+//         for(int j=1;j<=n+1-i;j++){
+//             printf("%d",j);
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+
+// enter a numbber 3
+// 123
+// 12
+// 1
+// ========================================================
+// #include<stdio.h>
+// int main(){
+//     int n;
+//     printf("enter a numbber ");
+//     scanf("%d",&n);
+//     for(int i=1;i<=n;i++){
+//         for(int j=1;j<=n;j++){------------------
+//             printf("%d",j);----------------------
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+
+
+// enter a numbber 4
+// 1234
+// 1234
+// 1234
+// 1234===================================================================
+
+
+
+// #include<stdio.h>
+// int main(){
+//     int n;
+//     printf("enter a numbber ");
+//     scanf("%d",&n);
+//     for(int i=1;i<=n;i++){
+//         for(int j=1;j<=i;j++){----------
+//             printf("%d",j);--------------------
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+// enter a numbber 4
+// 1
+// 12
+// 123
+// 1234=====================================================
+
+
+// #include<stdio.h>
+// int main(){
+//     int n;
+//     printf("enter a numbber ");
+//     scanf("%d",&n);
+//     for(int i=1;i<=n;i++){
+//         int a=1;
+//         for(int j=1;j<=n;j++){
+//             printf("%d",a);
+//             a=a+2;
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }=======================================================
+
+// enter a numbber 3
+// 135
+// 135
+// 135
+
+
+
+// #include<stdio.h>
+// int main(){
+//     int n;
+//     printf("enter a numbber ");
+//     scanf("%d",&n);
+//     for(int i=1;i<=n;i++){
+//         int a=1;
+//         for(int j=1;j<=i;j++){
+//             printf("%d",a);
+//             a=a+2;
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+// =========================================================
+// enter a numbber 6   
+// 1
+// 13
+// 135
+// 1357
+// 13579
+// 1357911
+// PS C:\Users\asusm\dsa folder> 
+
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,2 +1,13 @@
-
+#include<stdio.h>
+int main(){
+    int n;
+    printf("enter a numbber ");
+    scanf("%d",&n);
+    for(int i=0;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
 }
